5d6e735edd9bd9ee8b38cab587cb94018bd20b24
AdrianoSilva19 <ampsilva19@gmail.com>
2023-08-10 02:13:12 +0100

    [FIX] image inside the Card element

--- frontend/src/screens/LipidScreen.js
+++ frontend/src/screens/LipidScreen.js
@@ -29,20 +29,20 @@ function LipidScreen() {
       <Row style={{ fontSize: '40px', textAlign: 'center',marginTop: '20px', marginBottom: '20px' }}>
       <h3><strong>{lipid.name}</strong></h3>
       </Row>
-        <Row className="align-content-start">
-            <Col>
-            <Card className="my-3 p-3 rounded" border="Light" style={{ width: '100%', borderWidth: '1px', height: '17rem' }}>
-                {/* Display lipid image */}
-                <div style={{ width: '90%', height: '90%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                  <Card.Img
-                    data-smiles={lipid.smiles}
-                    data-smiles-options='{"maxWidth": "90%", "maxHeight": "90%","width": 1200, "height": 1200}'
-                    alt="Lipid Image"
-                  />
-                </div>
-              </Card>
-              </Col>              
-              </Row>
+      <Row className="align-content-start">
+        <Col>
+          <Card className="my-3 p-3 rounded" border="Light" style={{ width: '100%', borderWidth: '1px', height: '20rem' }}>
+            {/* Display lipid image */}
+            <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <Card.Img
+                data-smiles={lipid.smiles}
+                alt="Lipid Image"
+                style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
+              />
+            </div>
+          </Card>
+        </Col>
+      </Row>
               <Row className="align-content-start">
               <Col style={{marginTop: '15px'}}>
                       <ListGroup variant="secondary">
