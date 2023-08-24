73c6db25fbe2412c208f62b961dad402933253d7
AdrianoSilva19 <ampsilva19@gmail.com>
2023-08-24 14:23:40 +0100

    [ADD] pagination to Annotated list, and loading to Model screen

--- frontend/src/components/AnnotatedCard.js
+++ frontend/src/components/AnnotatedCard.js
@@ -1,37 +1,75 @@
-import React from 'react';
 import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function AnnotatedTable({ lipid }) {
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const data = Object.entries(lipid.annotated).map(([key, [lmValues, slValues, lipid_gema_id]]) => (
+    lmValues.map((lmValue, index) => ({
+      id: key,
+      lmValue,
+      slValue: slValues[index],
+      lipidGemaID: lipid_gema_id
+    }))
+  )).flat();
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const displayedData = data.slice(startIndex, endIndex);
+
   return (
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Lipid Maps ID</th>
-          <th>Swiss Lipids ID</th>
-          <th>Lipid Gema ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(lipid.annotated).map(([key, [lmValues, slValues,lipid_gema_id]]) => (
-          lmValues.map((lmValue, index) => (
+    <div>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Lipid Maps ID</th>
+            <th>Swiss Lipids ID</th>
+            <th>Lipid Gema ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayedData.map(({ id, lmValue, slValue, lipidGemaID }, index) => (
             <tr key={index}>
-              <td>{key}</td>
+              <td>{id}</td>
               <td>
                 <Link to={`https://www.lipidmaps.org/databases/lmsd/${lmValue}/`} target="_blank" style={{ textDecoration: 'none' }} >{lmValue}</Link>
               </td>
               <td>
-                <Link to={`https://www.swisslipids.org/#/entity/${slValues[index]}/`} target="_blank" style={{ textDecoration: 'none' }}>{slValues[index]}</Link>
+                <Link to={`https://www.swisslipids.org/#/entity/${slValue}/`} target="_blank" style={{ textDecoration: 'none' }}>{slValue}</Link>
               </td>
               <td>
-                <Link to={`/lipid/${lipid_gema_id}`} target="_blank" style={{ textDecoration: 'none' }}>{lipid_gema_id}</Link>
+                <Link to={`/lipid/${lipidGemaID}`} target="_blank" style={{ textDecoration: 'none' }}>{lipidGemaID}</Link>
               </td>
             </tr>
-          ))
-        ))}
-      </tbody>
-    </Table>
+          ))}
+        </tbody>
+      </Table>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <ul className="pagination">
+          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>
+              &laquo;
+            </button>
+          </li>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <li key={i} className={`page-item ${currentPage === i + 1 ? 'active' : ''}`}>
+              <button className="page-link" onClick={() => setCurrentPage(i + 1)}>
+                {i + 1}
+              </button>
+            </li>
+          ))}
+          <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+            <button className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>
+              &raquo;
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
--- frontend/src/screens/ModelScreen.js
+++ frontend/src/screens/ModelScreen.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Row } from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
+import ReactLoading from 'react-loading';
 import AnnotatedTable from '../components/AnnotatedCard'; 
 
 function ModelScreen() {
   const { model_id } = useParams();
   const [lipid, setLipid] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     async function fetchLipidData() {
       try {
         const response = await axios.get(`/api/model/${model_id}`);
         setLipid(response.data);
+        setIsLoading(false); // Set loading to false when data is fetched
       } catch (error) {
         console.error('Error fetching lipid data:', error);
       }
@@ -23,24 +26,37 @@ function ModelScreen() {
 
   return (
     <div>
-      <Row style={{ fontSize: '20px', textAlign: 'center', marginTop: '20px', marginBottom: '25px', color: 'black' }}>
-        
-
-
-      </Row>
-      {lipid && (
+      {isLoading ? ( // Render loading animation if still loading
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <Container style={{  marginBottom: '150px' }}>
+            <Row style={{ fontSize: '40px', textAlign: 'center' }}>
+              <strong>Organizing The Annotations</strong>
+            </Row>
+          </Container>
+          <ReactLoading
+            type="spinningBubbles"
+            color="#61a290"
+            height={150}
+            width={75}
+          />
+        </div>
+      ) : (
         <div>
-          <h3>Annotated</h3>
-          <AnnotatedTable lipid={lipid} /> {/* Use the new component */}
-          <h3>Suggested</h3>
-          <h6>Please select the right conformation on the <b>Suggested</b> annotations to annotate your <b>{model_id}</b> GSM model!</h6>
-          <ul>
-            {Object.keys(lipid.suggested).map((key) => (
-              <li key={key}>
-                {key}: {lipid.suggested[key]}
-              </li>
-            ))}
-          </ul>
+          {lipid && (
+            <div>
+              <h3>Annotated</h3>
+              <AnnotatedTable lipid={lipid} />
+              <h3>Suggested</h3>
+              <h6>Please select the right conformation on the <b>Suggested</b> annotations to annotate your <b>{model_id}</b> GSM model!</h6>
+              <ul>
+                {Object.keys(lipid.suggested).map((key) => (
+                  <li key={key}>
+                    {key}: {lipid.suggested[key]}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </div>
