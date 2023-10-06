4c9fd6f2ec5f1453ae592f9947656b09b2e2953d
AdrianoSilva19 <ampsilva19@gmail.com>
2023-10-06 15:24:35 +0100

    [ADD] documentation to the frontend components

--- frontend/src/components/Lipid.js
+++ frontend/src/components/Lipid.js
@@ -5,27 +5,37 @@ import { SmiDrawer } from 'smiles-drawer';
 
 
 function Lipid({lipid}) {
+    /**
+     * Renders a card with information about a lipid molecule.
+     * 
+     * @param {Object} lipid - An object containing information about a lipid molecule.
+     * @param {string} lipid.boimmg_id - The ID of the lipid.
+     * @param {string} lipid.name - The name of the lipid.
+     * @param {string} lipid.smiles - The SMILES representation of the lipid molecule.
+     * 
+     * @returns {JSX.Element} - A card component with the lipid molecule's name and image, wrapped in a link to a specific lipid page.
+     */
     useEffect(() => {
         SmiDrawer.apply(); // Call SmilesDrawer.apply() after rendering the component
-      }, [lipid]);
+    }, [lipid]);
 
-      return (
+    return (
         <Card className="my-3 p-3 rounded bg-light"  style={{ width: '20rem', borderWidth: '1px', height:'20rem' }}>
-          <Link to={`/lipid/${lipid.boimmg_id}`} style={{ textDecoration: 'none' }}>
-            <Card.Body className="d-flex flex-column align-items-center">
-              <Card.Title as="div" className="text-center mb-2">
-                <strong>{lipid.name}</strong>
-              </Card.Title>
-              <Card.Img
-                data-smiles={lipid.smiles}
-                data-smiles-options='{"width": 600, "height": 600}'
-                alt="Lipid Image"
-                className="align-self-center"
-              />
-            </Card.Body>
-          </Link>
+            <Link to={`/lipid/${lipid.boimmg_id}`} style={{ textDecoration: 'none' }}>
+                <Card.Body className="d-flex flex-column align-items-center">
+                    <Card.Title as="div" className="text-center mb-2">
+                        <strong>{lipid.name}</strong>
+                    </Card.Title>
+                    <Card.Img
+                        data-smiles={lipid.smiles}
+                        data-smiles-options='{"width": 600, "height": 600}'
+                        alt="Lipid Image"
+                        className="align-self-center"
+                    />
+                </Card.Body>
+            </Link>
         </Card>
-      );
-    }
+    );
+}
     
     export default Lipid;
